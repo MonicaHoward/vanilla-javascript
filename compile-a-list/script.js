a1ce67ea1c17f198a307ff7b710cb40d767e8639
Monica Howard <jay.m.howard@gmail.com>
2017-07-24 23:45:00 -0500

    add and delete buttons

---
+++ compile-a-list/script.js
@@ -0,0 +1,11 @@
+var addItem = document.querySelector('#add-item');
+var deleteItem = document.querySelector('#delete-item');
+var selectedItem;
+
+addItem.addEventListener('click', function() {
+  selectedItem;
+});
+
+deleteItem.addEventListener('click', function() {
+  selectedItem;
+});
